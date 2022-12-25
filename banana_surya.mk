5ac2ab17789b909741ec54ffbcf36263fe63c68d
TheStrechh <TheStrechh@gmail.com>
2022-12-25 21:06:57 +0000

    surya: banana-ify

--- banana_surya.mk
+++ banana_surya.mk
@@ -11,13 +11,23 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from surya device
 $(call inherit-product, device/xiaomi/surya/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Bananadroid stuff.
+$(call inherit-product, vendor/banana/config/common.mk)
 
-PRODUCT_NAME := lineage_surya
+PRODUCT_NAME := banana_surya
 PRODUCT_DEVICE := surya
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := POCO X3 NFC
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# QuickTap
+TARGET_SUPPORTS_QUICK_TAP := true
+
+# Faceunlock
+TARGET_FACE_UNLOCK_SUPPORTED := true
+
+# Maintainer
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.banana.maintainer=TheStrechh
